d6e787e4e88ec3e8045b1a13c53112f2aab73d57
unknown <azadam8255@gmail.com>
2021-07-11 00:13:31 +0800

    initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// firebase config
+export const firebaseConfig = {
+    apiKey: "AIzaSyBvbS075YwJU2lWFyQjWPKou9KFvoq6w38",
+    authDomain: "ecommerce-idham.firebaseapp.com",
+    projectId: "ecommerce-idham",
+    storageBucket: "ecommerce-idham.appspot.com",
+    messagingSenderId: "692592597346",
+    appId: "1:692592597346:web:6102a086f14f761487b43b",
+    measurementId: "G-HL075QM7KH"
+};
